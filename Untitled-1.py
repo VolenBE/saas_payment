3c8329d2b31d2806f7318a7fc0d526e43ec899cc
VolenBE <46224177+VolenBE@users.noreply.github.com>
2021-12-15 17:30:40 +0100

    requête 5

--- Untitled-1.py
+++ Untitled-1.py
@@ -17,19 +17,42 @@ cursor = dbase.cursor()
 company_id = 10
 
 cursor.execute('SELECT client_id FROM Clients WHERE company_id=?', [company_id])
-df = pd.DataFrame(cursor.fetchall(), columns=['ids'])
-client_ids = df['ids'].to_list()
-print(client_ids)
+main_df = pd.DataFrame(cursor.fetchall(), columns=['ids'])
+client_ids = main_df['ids'].to_list()
 rev_customers = []
+customers_name = []
+el_list = []
+name_list=[]
 
 for ids in client_ids:
-    cursor.execute('SELECT invoice_id, amount FROM Invoices WHERE client_id=?', [ids])
-    fetch = cursor.fetchone()
-    if fetch != None:
-        invoice_id = fetch[0]
-        amount = fetch[1]
-        rev_customers.append(amount)
+    cursor.execute('SELECT username FROM Users WHERE id=?', [ids])
+    customers_name.append(cursor.fetchone()[0])
+    cursor.execute('SELECT quote_id FROM Quotes WHERE client_id=?', [ids])
+    second_df = pd.DataFrame(cursor.fetchall(), columns=['Quotes ids'])
+    quote_ids = second_df['Quotes ids'].to_list()
+    print(quote_ids)
+    if quote_ids:
+        for qids in quote_ids:
+            cursor.execute('SELECT subscriptions_list FROM Quotes WHERE quote_id=?', [qids])
+            fetch_subs = json.loads(cursor.fetchone()[0])
+            for elements in fetch_subs:
+                cursor.execute('''
+                    SELECT name
+                    FROM Subscriptions
+                    WHERE subscription_id=?
+                ''', [elements])
+                name = cursor.fetchone()[0]
+                el_list.append(name)
+        name_list.append(el_list)
+    else:
+        name_list.append("No subscription")
 
-math = sum(rev_customers) / len(rev_customers)
-print(math)
+print(name_list)
+
+main_df['Names'] = customers_name # columns names
+main_df['Subscriptions'] = name_list #column subs
+print(main_df)
+
+#mean = sum(rev_customers) / len(rev_customers)
+#return {"message": "The average revenue per customer is: {average_revenue}".format(average_revenue = mean)}
 
--- main.py
+++ main.py
@@ -366,6 +366,41 @@ async def customer_subs(payload: Request):
   dbase = sqlite3.connect('database.db', isolation_level=None)
   cursor = dbase.cursor()
   
+  cursor.execute('SELECT client_id FROM Clients WHERE company_id=?', [company_id])
+  main_df = pd.DataFrame(cursor.fetchall(), columns=['ids'])
+  client_ids = main_df['ids'].to_list()
+  customers_name = []
+  el_list = []
+  name_list=[]
+
+  for ids in client_ids:
+      cursor.execute('SELECT username FROM Users WHERE id=?', [ids])
+      customers_name.append(cursor.fetchone()[0])
+      cursor.execute('SELECT quote_id FROM Quotes WHERE client_id=?', [ids])
+      second_df = pd.DataFrame(cursor.fetchall(), columns=['Quotes ids'])
+      quote_ids = second_df['Quotes ids'].to_list()
+      print(quote_ids)
+      if quote_ids:
+          for qids in quote_ids:
+              cursor.execute('SELECT subscriptions_list FROM Quotes WHERE quote_id=?', [qids])
+              fetch_subs = json.loads(cursor.fetchone()[0])
+              for elements in fetch_subs:
+                  cursor.execute('''
+                      SELECT name
+                      FROM Subscriptions
+                      WHERE subscription_id=?
+                  ''', [elements])
+                  name = cursor.fetchone()[0]
+                  el_list.append(name)
+          name_list.append(el_list)
+      else:
+          name_list.append("No subscription")
+
+  print(name_list)
+
+  main_df['Names'] = customers_name # columns names
+  main_df['Subscriptions'] = name_list #column subs
+  print(main_df)
 
 # Server launch and reset invoices if last day of the month 
 
